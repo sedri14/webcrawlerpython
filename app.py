c6e7731012c73ee9c2eb13e141921ac74c83f2d4
Sedri14 <sedri14@gmail.com>
2023-09-16 14:05:18 +0300

    recursive scrape function

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 
-URL = 'https://en.wikipedia.org/wiki/Cat'
+URL = 'https://www.geeksforgeeks.org/image-scraping-with-python/'
 
 
 def get_data(url):
@@ -10,17 +11,34 @@ def get_data(url):
     return soup
 
 
-def print_images(soup):
+def print_images(soup, source_url, depth):
     images = soup.findAll('img')
     for img in images:
-        print(img)
+        img_url = img.get('src')
+        print(f'sourceUrl: {source_url}')
+        print(f'imageUrl: {img_url}')
+        print(f'depth: {depth}')
+        print('******************************')
 
 
-def scrape():
-    soup = get_data(URL)
-    print_images(soup)
+def scrape(url, cur_depth, limit_depth):
+    if limit_depth == 0 or cur_depth == limit_depth:
+        soup = get_data(url)
+        print_images(soup, url, limit_depth)
+        return
+
+    soup = get_data(url)
+    print_images(soup, url, cur_depth)
+
+    # get all links in page
+    links = soup.findAll('a')
+    for link in links:
+        relative_url = link['href']
+        absolute_url = urljoin(url, relative_url)
+        scrape(absolute_url, cur_depth + 1, limit_depth)
 
 
 if __name__ == '__main__':
-    scrape()
+
+    scrape(URL, 0, 1)
     print('finished')
