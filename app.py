f935691c60ae4201f5b87e5ca2a2c6b91a98a6a4
Sedri14 <sedri14@gmail.com>
2023-09-16 12:56:07 +0300

    extract images from initial url

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('hi')
+from bs4 import BeautifulSoup
+import requests
+
+URL = 'https://en.wikipedia.org/wiki/Cat'
+
+
+def get_data(url):
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    return soup
+
+
+def print_images(soup):
+    images = soup.findAll('img')
+    for img in images:
+        print(img)
+
+
+def scrape():
+    soup = get_data(URL)
+    print_images(soup)
+
+
+if __name__ == '__main__':
+    scrape()
+    print('finished')
