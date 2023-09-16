32f304f5cb8e173b7195ecf44487b86b41b131e9
Sedri14 <sedri14@gmail.com>
2023-09-16 14:52:07 +0300

    write results to a json file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import requests
+import json
+
 
 URL = 'https://www.geeksforgeeks.org/image-scraping-with-python/'
 
@@ -11,34 +13,44 @@ def get_data(url):
     return soup
 
 
-def print_images(soup, source_url, depth):
+def extract_images(soup, source_url, depth, image_data_list):
     images = soup.findAll('img')
     for img in images:
         img_url = img.get('src')
-        print(f'sourceUrl: {source_url}')
-        print(f'imageUrl: {img_url}')
-        print(f'depth: {depth}')
-        print('******************************')
+        img_data = {
+            "sourceUrl": source_url,
+            "imageUrl": img_url,
+            "depth": depth
+        }
+        image_data_list.append(img_data)
 
 
-def scrape(url, cur_depth, limit_depth):
+def scrape(url, cur_depth, limit_depth, image_data_list):
     if limit_depth == 0 or cur_depth == limit_depth:
         soup = get_data(url)
-        print_images(soup, url, limit_depth)
+        extract_images(soup, url, limit_depth, image_data_list)
         return
 
     soup = get_data(url)
-    print_images(soup, url, cur_depth)
+    extract_images(soup, url, cur_depth, image_data_list)
 
     # get all links in page
     links = soup.findAll('a')
     for link in links:
         relative_url = link['href']
         absolute_url = urljoin(url, relative_url)
-        scrape(absolute_url, cur_depth + 1, limit_depth)
+        # todo: check for previously visited urls
+        scrape(absolute_url, cur_depth + 1, limit_depth, image_data_list)
 
 
 if __name__ == '__main__':
+    img_data_list = []
+    scrape(URL, 0, 1, img_data_list)
 
-    scrape(URL, 0, 1)
+    # write results to a json file
+    data_to_save = {
+        "results": img_data_list
+    }
+    with open('results.json', 'w') as json_file:
+        json.dump(data_to_save, json_file, indent=4)
     print('finished')
